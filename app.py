3811cc1a23782ea69605317970146e79506beed8
varunjaggi <48566633+varunjaggi@users.noreply.github.com>
2020-06-17 18:05:19 +0530

    update insert func

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request
+from firebase import insert
 
 app=Flask(__name__)
 
@@ -16,6 +17,7 @@ def thankyou():
     first=request.args.get('first')
     last=request.args.get('last')
     age=request.args.get('age')
+    insert(first,last,age)
     return render_template("thankyou.html",first=first)
 
 if __name__=='__main__':
