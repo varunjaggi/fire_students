aeda3ccbb5e3ab5b495d6f47393c2defcfb149c1
varunjaggi <48566633+varunjaggi@users.noreply.github.com>
2020-06-17 17:48:56 +0530

    firebase heroku version

---
+++ firebase.py
@@ -0,0 +1,25 @@
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+class Student(object):
+    def __init__(self, first, last, born):
+        self.first = first
+        self.last = last
+        self.age = born
+
+    def to_dict(self):
+        return self.__dict__
+
+cred = credentials.Certificate("/media/varunjaggi/DriveE/firestudents/firestudents-7dc4f-firebase-adminsdk-qic8w-960dc37f43.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+doc_ref = db.collection(u'students').document()
+
+
+def insert(first,last,age): #function for inserting to DB
+  student = Student(first,last,age)
+  doc_ref.set(student.to_dict())
+
+
+
